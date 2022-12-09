06e4228f27765e755b37b3fad43b84b58f124cd9
njoroge-s <sarahnjoroge2015@gmail.com>
2022-12-09 14:36:26 +0300

    my calculator

---
+++ calc.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+
+LIGHT_GRAY = "#F5F5F5"
+
+class Calculator:
+    def __int__(self):
+        self.window = tk.Tk()
+        self.window.geometry("375 × 667")
+        self.window.resizable(0, 0)
+        self.window.title("Calculator")
+
+        # display and button frames
+        self.display_frame = self.create_display_frame()
+        self.buttons_frame = self.create_buttons_frame()
+
+    def create_display_frame(self):
+        frame = tk.Frame(self.window, height=221, bg = LIGHT_GRAY)
+        frame.pack(expand=True, fill="both")
+        return frame
+
+    def create_buttons_frame()
+
+    def run(self):
+        self.window.mainloop()
+
+
+if __name__ == "__main__":
+    calc = Calculator()
+    calc.run()
